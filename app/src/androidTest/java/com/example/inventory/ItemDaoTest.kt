274ad36e310654e12a74fe6ae8dc3f9173d6b152
axelr <axelrepeat@gmail.com>
2025-10-15 15:46:20 -0600

    Se agregó una prueba para la consulta de DAO de obtención de elementos y se añadió propragación de texto y la pantalla Edit Item con los detalles de la entidad

--- app/src/androidTest/java/com/example/inventory/ItemDaoTest.kt
+++ app/src/androidTest/java/com/example/inventory/ItemDaoTest.kt
@@ -76,6 +76,15 @@ class ItemDaoTest {
         assertTrue(allItems.isEmpty())
     }
 
+    @Test
+    @Throws(Exception::class)
+    fun daoGetItem_returnsItemFromDB() = runBlocking {
+        addOneItemToDb()
+        val item = itemDao.getItem(1)
+        assertEquals(item.first(), item1)
+    }
+
+
     @Before
     fun createDb() {
         val context: Context = ApplicationProvider.getApplicationContext()
