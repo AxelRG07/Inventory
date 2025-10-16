77447a488280158a16ad93a111f768355396a928
axelr <axelrepeat@gmail.com>
2025-10-15 20:15:57 -0600

    Se completó la función para actualizar items en la base de datos mediante el viewModel actualizando el estado y usando una función de suspensión para hacer operaciones en la BD.

--- app/src/main/java/com/example/inventory/ui/item/ItemDetailsScreen.kt
+++ app/src/main/java/com/example/inventory/ui/item/ItemDetailsScreen.kt
@@ -141,7 +141,7 @@ private fun ItemDetailsBody(
             onClick = onSellItem,
             modifier = Modifier.fillMaxWidth(),
             shape = MaterialTheme.shapes.small,
-            enabled = itemDetailsUiState.outOfStock
+            enabled = !itemDetailsUiState.outOfStock
         ) {
             Text(stringResource(R.string.sell))
         }
